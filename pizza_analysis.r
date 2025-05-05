6280b896dcd0703d8f5e73e4d29c011bf655af72
Ellen Whalen <ellenewhalen@gmail.com>
2025-05-05 11:33:28 -0700

    loops that do the million 2x2s

--- pizza_analysis.r
+++ pizza_analysis.r
@@ -24,68 +24,104 @@ clean_data <- pizza_data %>%
 #     success_rate = mean(received_pizza)
 #   )
 
-
-hour <- c(hour(clean_data$request_time))
-
-totalpizza <- sum(clean_data$successes)
-ftotalpizza <- sum(clean_data$failures)
-
-h0successrate <- totalpizza / (ftotalpizza + totalpizza)
+# REUSED VALUES
+hours <- c(hour(clean_data$request_time))
+totalsuccesses <- sum(clean_data$successes)
+totalfailures <- sum(clean_data$failures)
+h0successrate <- totalsuccesses / (totalfailures + totalsuccesses)
 h0failurerate <- 1 - h0successrate
 
-pizzatimes <- c()
-fpizzatimes <- c()
+# ---------------------------------------- 1h bins ----------------------------------------
+succ_1h <- c()
+fail_1h <- c()
+for (i in 0:23) {
+  succ_1h <- c(succ_1h, sum(clean_data$successes[hours==i]))
+  fail_1h <- c(fail_1h, sum(clean_data$failures[hours==i]))
+}
+
+totals_1h <- c()
+for (i in 1:length(succ_1h)) {
+  totals_1h <- c(totals_1h, succ_1h[i] + fail_1h[i])
+}
+
+csq_1h <- 0
+for (i in 1:length(succ_1h)) {
+  s_exp_1h <- totals_1h[i] * h0successrate
+  f_exp_1h <- totals_1h[i] * h0failurerate
+  csq_1h <- csq_1h + ((succ_1h[i] - s_exp_1h) ^ 2) / s_exp_1h
+  csq_1h <- csq_1h + ((fail_1h[i] - f_exp_1h) ^ 2) / f_exp_1h
+}
+
+prob_timeofday_1h <- pchisq(csq_1h, 11, lower.tail = FALSE)
+# -----------------------------------------------------------------------------------------
+# ---------------------------------------- 2h bins ----------------------------------------
+succ_2h <- c()
+fail_2h <- c()
 for (i in 0:11) {
-  pizzatimes <- c(pizzatimes, sum(clean_data$successes[hour >= (2 * i) & hour < (2 * i) + 2]))
-  fpizzatimes <- c(fpizzatimes, sum(clean_data$failures[hour >= (2 * i) & hour < (2 * i) + 2]))
+  succ_2h <- c(succ_2h, sum(clean_data$successes[hours >= (2 * i) & hours < (2 * i) + 2]))
+  fail_2h <- c(fail_2h, sum(clean_data$failures[hours >= (2 * i) & hours < (2 * i) + 2]))
 }
 
-pizzatotalstwo <- c()
-for (i in 1:length(pizzatimes)) {
-  pizzatotalstwo <- c(pizzatotalstwo, pizzatimes[i] + fpizzatimes[i])
+totals_2h <- c()
+for (i in 1:length(succ_2h)) {
+  totals_2h <- c(totals_2h, succ_2h[i] + fail_2h[i])
 }
 
-csqtwo <- 0
-for (i in 1:length(pizzatimes)) {
-  s_expected <- pizzatotalstwo[i] * h0successrate
-  f_expected <- pizzatotalstwo[i] * h0failurerate
-  csqtwo <- csqtwo + ((pizzatimes[i] - s_expected) ^ 2) / s_expected
-  csqtwo <- csqtwo + ((fpizzatimes[i] - f_expected) ^ 2) / f_expected
+csq_2h <- 0
+for (i in 1:length(succ_2h)) {
+  s_exp_2h <- totals_2h[i] * h0successrate
+  f_exp_2h <- totals_2h[i] * h0failurerate
+  csq_2h <- csq_2h + ((succ_2h[i] - s_exp_2h) ^ 2) / s_exp_2h
+  csq_2h <- csq_2h + ((fail_2h[i] - f_exp_2h) ^ 2) / f_exp_2h
 }
 
-prob_timeofday_twohr <- pchisq(csqtwo, 11, lower.tail = FALSE)
+prob_timeofday_2h <- pchisq(csq_2h, 11, lower.tail = FALSE)
+# -----------------------------------------------------------------------------------------
+# ---------------------------------------- 4h bins ----------------------------------------
 
-earlypizza <- sum(clean_data$successes[hour <= 4])
-morningpizza <- sum(clean_data$successes[hour > 4 & hour <= 8])
-breakfastpizza <- sum(clean_data$successes[hour > 8 & hour <= 12])
-middaypizza <- sum(clean_data$successes[hour > 12 & hour <= 16])
-dinnerpizza <- sum(clean_data$successes[hour > 16 & hour <= 20])
-eveningpizza <- sum(clean_data$successes[hour > 20])
-pizzasuccesses <- c(earlypizza, morningpizza, breakfastpizza, middaypizza, dinnerpizza, eveningpizza)
-
-fearlypizza <- sum(clean_data$failures[hour <= 4])
-fmorningpizza <- sum(clean_data$failures[hour > 4 & hour <= 8])
-fbreakfastpizza <- sum(clean_data$failures[hour > 8 & hour <= 12])
-fmiddaypizza <- sum(clean_data$failures[hour > 12 & hour <= 16])
-fdinnerpizza <- sum(clean_data$failures[hour > 16 & hour <= 20])
-feveningpizza <- sum(clean_data$failures[hour > 20])
-pizzafailures <- c(fearlypizza, fmorningpizza, fbreakfastpizza, fmiddaypizza, fdinnerpizza, feveningpizza)
-
-pizzatotals <- c()
-for (i in 1:length(pizzasuccesses)) {
-  pizzatotals <- c(pizzatotals, pizzasuccesses[i] + pizzafailures[i])
+succ_4h <- c()
+fail_4h <- c()
+for (i in 0:5) {
+  succ_4h <- c(succ_4h, sum(clean_data$successes[hours >= (4 * i) & hours < (4 * i) + 4]))
+  fail_4h <- c(fail_4h, sum(clean_data$failures[hours >= (4 * i) & hours < (4 * i) + 4]))
 }
 
-h0successrate <- totalpizza / (ftotalpizza + totalpizza)
-h0failurerate <- 1 - h0successrate
-
-csq <- 0
-for (i in 1:length(pizzasuccesses)) {
-  s_expected <- pizzatotals[i] * h0successrate
-  f_expected <- pizzatotals[i] * h0failurerate
-  csq <- csq + ((pizzasuccesses[i] - s_expected) ^ 2) / s_expected
-  csq <- csq + ((pizzafailures[i] - f_expected) ^ 2) / f_expected
+totals_4h <- c()
+for (i in 1:length(succ_4h)) {
+  totals_4h <- c(totals_4h, succ_4h[i] + fail_4h[i])
 }
 
-prob_timeofday <- pchisq(csq, 5, lower.tail = FALSE)
+csq_4h <- 0
+for (i in 1:length(succ_4h)) {
+  s_exp_4h <- totals_4h[i] * h0successrate
+  f_exp_4h <- totals_4h[i] * h0failurerate
+  csq_4h <- csq_4h + ((succ_4h[i] - s_exp_4h) ^ 2) / s_exp_4h
+  csq_4h <- csq_4h + ((fail_4h[i] - f_exp_4h) ^ 2) / f_exp_4h
+}
+
+prob_timeofday_4h <- pchisq(csq_4h, 11, lower.tail = FALSE)
+# -----------------------------------------------------------------------------------------
+
+# 2x2 chi-square tests
+results <- c()
+for (i in 1:length(succ_4h)) {
+  for (j in 1:length(succ_4h)) {
+    if (i >= j) {
+      next
+    }
+    this_ts <- 0
+    thish0succ <- (succ_4h[i] + succ_4h[j]) / (succ_4h[i] + succ_4h[j] + fail_4h[i] + fail_4h[j])
+    thish0fail <- 1 - thish0succ
+    s_exp_i <- totals_4h[i] * thish0succ
+    f_exp_i <- totals_4h[i] * thish0fail
+    s_exp_j <- totals_4h[j] * thish0succ
+    f_exp_j <- totals_4h[j] * thish0fail
+    this_ts <- this_ts + ((succ_4h[i] - s_exp_i) ^ 2) / s_exp_i
+    this_ts <- this_ts + ((succ_4h[j] - s_exp_j) ^ 2) / s_exp_j
+    this_ts <- this_ts + ((fail_4h[i] - f_exp_i) ^ 2) / f_exp_i
+    this_ts <- this_ts + ((fail_4h[j] - f_exp_j) ^ 2) / f_exp_j
+    results <- c(results, c(i, j, pchisq(this_ts, 1, lower.tail=FALSE)))
+  }
+}
+
 
